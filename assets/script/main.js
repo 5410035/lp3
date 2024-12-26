04b1810fae6efa2345bb46c4dd4f4809e7c64e7b
5410035 <5410035sryuji331@gmail.com>
2024-12-26 18:54:25 +0900

    20241226_メンタリング前

---
+++ assets/script/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(".voice__container-customer-slick").slick({
+        arrows: true,
+        autoplay: false,
+        adaptiveHeight: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        initialSlide: 1,
+        centerMode: true,
+        centerPadding: "2.3rem",
+    });
+});
